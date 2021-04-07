4ba5e706a31e61a5d683dd8bbc32fc1ebb8b9178
Meryembentalebeljadil <meryembentaleb11@gmail.com>
2021-04-07 09:25:54 +0200

    js-exo1-past2

---
+++ main.js
@@ -0,0 +1,21 @@
+function setNewImage(){
+    document.getElementById('img1').src ="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo6-js/images/41y16B5C6rL._SX311_BO1,204,203,200_.jpg";
+} 
+function setImage(){
+    document.getElementById('img2').src="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo6-js/images/clare_paint__zoom_backgrounds_Current_Mood.jpg"
+
+}
+function Image(){
+    document.getElementById('img3').src="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo6-js/images/images.jpeg"
+}
+
+function setOldOne(){
+    document.getElementById('img1').src ="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo6-js/images/10 Zoom Backgrounds HubSpotters Love-Aug-31-2020-07-31-22-14-AM.jpeg";
+} 
+function setOldImage(){
+    document.getElementById('img2').src="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo6-js/images/bg-picture.jpg"
+
+}
+function setOldImages(){
+    document.getElementById('img3').src="/home/bentaleb/Desktop/exercice/JavaScript/part 2/exo6-js/images/grumpy_cat.jpg"
+}
